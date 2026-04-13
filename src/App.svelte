677844f956d5e25464bf6bc84cd46de116d9a401
xin <98406118+9c5s@users.noreply.github.com>
2026-04-13 19:33:49 +0900

    feat: LayoutSwitcherとApp.svelteに組み込む

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
   import Dashboard from "./components/Dashboard.svelte";
   import CardsLayout from "./components/CardsLayout.svelte";
   import TableLayout from "./components/TableLayout.svelte";
+  import PlayerStatusLayout from "./components/player-status/PlayerStatusLayout.svelte";
   import LayoutSwitcher from "./components/LayoutSwitcher.svelte";
 
   $effect(() => {
@@ -23,6 +24,8 @@
   <CardsLayout />
 {:else if store.layoutMode === "table"}
   <TableLayout />
+{:else if store.layoutMode === "player-status"}
+  <PlayerStatusLayout />
 {:else}
   <Dashboard />
 {/if}
